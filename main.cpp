4bf2991799a618c23d3062752b1721797df05e4a
Pinkii- <pinkiibiza@gmail.com>
2015-01-16 11:47:04 +0100

    -Includes de las librerias -Declarar y abrir la ventana donde se ejecutará el juego -Presentar el bucle principal del juego -Hacer el draw basico (Limpiar el anterior frame y mostrar en pantalla lo que hay dibujado en la window)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 
 int main() {
-	return 0;
+  sf::RenderWindow window(sf::VideoMode(1280,720), "My Pang", sf::Style::Default);
+  while(window.isOpen()) { //Bucle del Juego
+    // Procesar Eventos
+
+    // Updatear el juego
+
+    // Dibujar el juego
+    window.clear(sf::Color(100,100,100));
+    // Aquí se hará el draw de cada elemento
+    window.display();
+  }
+  return 0;
 }
